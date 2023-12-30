8fc206fca518988b061d56b057d6b88c60251dee
patharharsh <patharharsh@gmail.com>
2023-12-30 16:49:09 +0530

    first commit

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const Home = () => {
+const Navbar = () => {
   return (
     <div>
-      Hello
+      Navbar
     </div>
   )
 }
 
-export default Home
+export default Navbar
