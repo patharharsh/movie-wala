7f997d99c34a97b128a375090db9ba2519146892
patharharsh <patharharsh@gmail.com>
2024-01-16 12:28:53 +0530

    section title added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import HeroSection from '@/components/herosection/HeroSection'
+import SectionTitle from '@/components/sectionTitle/sectionTitle'
 import React from 'react'
 
 const Home = () => {
   return (
     <div>
       <HeroSection />
+      <SectionTitle>New Arrival</SectionTitle>
     </div>
   )
 }
